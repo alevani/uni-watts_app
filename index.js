15dd472093c4b32edd742e77554f98750d1d9f7d
alevani <vanini.alexandre@gmail.com>
2020-10-07 21:03:17 +0200

    Add dotenv support

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 "use strict"
 
+require('dotenv').config()
 const express = require('express');
-const dbConnector = require('./database/Connector');
 const app = express();
+
+const dbConnector = require('./database/Connector');
 const port = 8080;
-
 const endpoint = 'watts'
-
-// Database info
-//TODO move the below to a util file
-const url = 'mongodb://localhost:27017';
-const dbName = 'myproject';
-
-const conn = new dbConnector(url, dbName);
+console.log(process.env.DB_HOST);
+const conn = new dbConnector(process.env.DB_HOST, process.env.DB_NAME);
 
 // Example callback wrapper
-const addUser = (x) => {
-    return function (err, client) {
-        console.log(x);
-        const db = client.db('watts');
-        /*
-        do something
-        */
-        client.close();
-    }
-}
+// const addUser = (x) => {
+//     return function (err, client) {
+//         console.log(x);
+//         const db = client.db('watts');
+//         /*
+//         do something
+//         */
+//         client.close();
+//     }
+// }
 
-
-conn.execute(addUser('jerome'));
-conn.addUser('jerome');
+// conn.execute(addUser('jerome'));
+// conn.addUser('jerome');
 
 app.get('/', (req, res) => {
     res.json('Hello world');
