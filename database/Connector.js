f0f300d64b821a68be852ce4ccae0d72d172e68c
alevani <vanini.alexandre@gmail.com>
2020-10-07 21:03:33 +0200

    Adduser dummy function to show wrapper use

--- database/Connector.js
+++ database/Connector.js
@@ -4,15 +4,22 @@ const MongoClient = require('mongodb').MongoClient;
 module.exports = class Connector {
 
     constructor(url, dbName) {
+        console.log(url);
         this.url = url;
         this.dbName = dbName;
     }
 
-    connect() {
-        MongoClient.connect(this.url, { useUnifiedTopology: true }, function (err, client) {
-            console.log("Connected successfully to server");
-            const db = client.db('dwwa');
+    execute(callback) {
+        MongoClient.connect(this.url, { useUnifiedTopology: true }, callback);
+    }
 
+    addUser(x) {
+        MongoClient.connect(this.url, { useUnifiedTopology: true }, function (err, client) {
+            console.log(x);
+            const db = client.db('watts');
+            /*
+            do something
+            */
             client.close();
         });
     }
