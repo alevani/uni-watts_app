a1a3484e5c3a1e0f73908fd2786160c667886046
alevani <vanini.alexandre@gmail.com>
2020-10-07 21:09:56 +0200

    Add route

--- index.js
+++ index.js
@@ -29,8 +29,9 @@ app.get('/', (req, res) => {
     res.json('Hello world');
 });
 
-// app.get(`${endpoint}/friends`)
 
+// app.get(`${endpoint}/friends`)
+// app.post(`${endpoint}/friends`)
 app.listen(port, () => {
     console.log(`Started listening on port ${port}`);
 });
